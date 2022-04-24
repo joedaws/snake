4aea1a4f4ff494e2439911ec923077ac111e4bb9
Joseph Daws <daws.joseph@gmail.com>
2022-04-24 16:11:27 -0400

    added notes about user input and distribution

--- lib/player_input.py
+++ lib/player_input.py
@@ -3,11 +3,13 @@
 from enum import Enum, auto
 
 
-class DPadDirection(Enum):
+class PlayerInput(Enum):
     UP = auto()
     DOWN = auto()
     RIGHT = auto()
     LEFT = auto()
+    SELECT = auto()
+    PAUSE = auto()
 
 
 class InputListener:
@@ -21,10 +23,24 @@ class InputListener:
     TODO add a pause button feature.
     """
     def __init__(self):
-        self.dpad_state
+        self.dpad_state = None
+
+        # TODO use strategy design pattern to
+        #    allow for different user input
+        #    configurations
 
     def listen(self):
         """Get player input and update DPadState"""
+        # TODO read about to continuously get user input
+        player_input = None
+        if player_input == 'w':
+            self.dpad_state = PlayerInput.UP
+        if player_input == 's':
+            self.dpad_state = PlayerInput.DOWN
+        if player_input == 'a':
+            self.dpad_state = PlayerInput.LEFT
+        if player_input == 'd':
+            self.dpad_state = PlayerInput.RIGHT
 
 
 class PlayerAction:
--- lib/test_input.py
+++ lib/test_input.py
@@ -1,10 +1,18 @@
 #!/usr/bin/env python3
+from threading import Thread, Timer
+import time
+
+
+
 
 def main():
-    # get input
-    char = None
-    while char != 'q':
-        char = eval(input())
+    """Get input of user from another thread."""
+    timeout = 10
+    t = Timer(timeout, print, ['Sorry, times up'])
+    t.start()
+    prompt = f"You have {timeout} seconds to choose the correct answer...\n"
+    answer = input(prompt)
+    t.cancel()
 
 
 if __name__ == "__main__":
