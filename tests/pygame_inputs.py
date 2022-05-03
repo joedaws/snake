a1bdb3c2b38fdf32f10162c8b64da04a4a5feb7f
Joseph Daws <daws.joseph@gmail.com>
2022-05-02 23:00:32 -0400

    made hello world animation with pygame

--- tests/pygame_inputs.py
+++ tests/pygame_inputs.py
@@ -1,47 +1,80 @@
 #!/usr/bin/env python3
 
 import sys
-import time
+import math
 import pygame
+from dataclasses import dataclass
 
 pygame.init()
 
 clock = pygame.time.Clock()
 
-screen = pygame.display.set_mode([600,500])
+screen = pygame.display.set_mode([300, 300])
 
 base_font = pygame.font.Font(None, 32)
 user_txt = ''
 
-input_rect = pygame.Rect(200, 200, 140, 32)
+left = screen.get_width() // 2 - screen.get_width() // 4
+top = screen.get_height() // 2 - screen.get_height() // 4
+input_rect = pygame.Rect(left, top, 100, 100)
 
-color = pygame.Color('lightskyblue3')
+color = pygame.Color('white')
+
+# width between animated characters
+W_FACTOR = 20
+
+# factor of pi to use to step between
+WOBBLE_STEP = math.pi / (24)
+
+FRAME_RATE = 24
+
+DARK_PURPLE = (82, 28, 102)
+
+
+@dataclass
+class PixelPoint:
+    x: int
+    y: int
+
+
+p1 = PixelPoint(input_rect.x, input_rect.y)
+
 
 def main():
+    i = 0
     while True:
+        clock.tick(FRAME_RATE)
+
+        i += 1
+        if i == 1000*FRAME_RATE:
+            i = 0
+
         for event in pygame.event.get():
-            if event.type == pygame.KEYDOWN:
+            if event.type == pygame.KEYDOWN or event.type == pygame.QUIT:
                 if event.key == pygame.K_SPACE:
                     pygame.quit()
                     sys.exit()
 
-        screen.fill((255, 255, 255))
+        screen.fill(DARK_PURPLE)
 
-        pygame.draw.rect(screen, color, input_rect)
+        pygame.draw.rect(screen, DARK_PURPLE, input_rect)
 
         user_text = "Hello"
 
-        text_surface = base_font.render(user_text,
-                                        True,
-                                        (255, 255, 255))
-
-        screen.blit(text_surface, (input_rect.x+5, input_rect.y+5))
+        # draw hello world a few pixels apart
+        for j, letter in enumerate(user_text):
+            x = p1.x + j * W_FACTOR + 3
+            y = p1.y + int(12*math.sin(0.5*(i-j)*WOBBLE_STEP)) + 50
+            text_surface = base_font.render(user_text[j],
+                                            True,
+                                            (255, 255, 255))
+            screen.blit(text_surface, (x, y))
 
         input_rect.w = max(100, text_surface.get_width()+10)
+        input_rect.h = max(100, text_surface.get_height()+10)
 
         pygame.display.flip()
 
-        clock.tick(60)
 
 if __name__ == "__main__":
     main()
